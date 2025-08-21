b0ea97fd0ac32165cdfd78c4d24c7afc34a7433d
João Victor Dias <jotavaresvictor@gmail.com>
2025-08-21 20:04:51 -0300

    fix a type search params

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,27 +1,28 @@
 import { prisma } from "@/libs/prisma";
 
+
 interface PostPageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>
 }
 
 export default async function PostPage({ params }: PostPageProps) {
-  const post = await prisma.post.findUnique({ where: { id: params.id } });
+  const { id } = await params
+  const post = await prisma.post.findUnique({ where: { id } });
 
   if (!post) return <p>Post não encontrado</p>;
 
   return (
     <main className="mt-16 px-6 max-w-3xl mx-auto prose prose-slate prose-lg dark:prose-invert">
-      {/* Título */}
+
       <h1 className="text-5xl font-extrabold mb-4 text-center text-gray-900 dark:text-gray-700">
         {post.title}
       </h1>
 
-      {/* Data do post */}
+
       <p className="text-sm text-gray-500 text-center mb-8">
         {new Date(post.createdAt).toLocaleDateString("pt-BR")}
       </p>
 
-      {/* Conteúdo */}
       <div className="space-y-6">
         {post.content.split("\n").map((line, idx) => (
           <p key={idx}>{line}</p>
@@ -29,4 +30,4 @@ export default async function PostPage({ params }: PostPageProps) {
       </div>
     </main>
   );
-}
+};
